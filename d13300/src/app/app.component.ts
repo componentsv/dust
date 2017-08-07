8e812d7370e503a5d78f82c39ab7e5913717978e
anonymoussc <50c5ac69@opayq.com>
2017-08-07 19:16:14 +0700

    Updated d13300

--- d13300/src/app/app.component.ts
+++ d13300/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selection = 'Name';
+  options = ['name', 'address', 'other']
 }

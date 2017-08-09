5f8d5ebd24dd1170991620ee4104af898e37cae2
anonymoussc <50c5ac69@opayq.com>
2017-08-10 04:54:32 +0700

    Updated d14300

--- d14300/src/app/app.module.ts
+++ d14300/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule

308b82bd06b51b55e963d472c383fac14f4f7679
anonymoussc <50c5ac69@opayq.com>
2017-08-11 02:59:48 +0700

    Updated d14700

--- d14700/src/app/app.component.ts
+++ d14700/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+  
+  @ViewChildren('paragraph1, paragraph2') paragraphs;
+  
+  note:string = '';
+
+  ngAfterViewInit() {
+    setTimeout(_ => this.note = 'Number of Paragraphs : ' + this.paragraphs.length);
+  }
 }

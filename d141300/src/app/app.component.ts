6074e29e90d9f30b984143c99665b82d53358b46
anonymoussc <50c5ac69@opayq.com>
2017-08-14 05:57:11 +0700

    Updated d141300

--- d141300/src/app/app.component.ts
+++ d141300/src/app/app.component.ts
@@ -1,4 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, DoCheck, IterableDiffers } from '@angular/core';
+
+@Component({
+  selector: 'numbers',
+  template: `
+    {{numbers}}
+    <br />
+    <p *ngFor="let change of changes">
+      {{change}}
+    </p>
+  `,
+  styles:['p{padding:0;margin:0}']
+})
+export class NumbersComponent implements DoCheck {
+  @Input('numbers') numbersArray: Array<string>; 
+  changes: Array<string> = [];
+  differ;
+
+  constructor(private differs: IterableDiffers) {
+    this.differ = differs.find([]).create(null); 
+  }
+
+  ngDoCheck() {
+    const differences = this.differ.diff(this.numbersArray); 
+    
+    if (differences) {
+      if (differences.forEachAddedItem) {
+        differences.forEachAddedItem((item) => {
+          if ((item) && (item.item)) {
+            this.changes.push('added ' + item.item); }
+        });
+      }
+      
+      if (differences.forEachRemovedItem) {
+        differences.forEachRemovedItem((item) => {
+          if ((item) && (item.item)) {
+            this.changes.push('removed ' + item.item); }
+        });
+      }
+    }
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +47,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  numbers = '';
 }

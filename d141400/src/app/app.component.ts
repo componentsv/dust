d4df12686983a1b500652db106132b0fcf7acd36
anonymoussc <50c5ac69@opayq.com>
2017-08-14 17:45:41 +0700

    Updated d141400

--- d141400/src/app/app.component.ts
+++ d141400/src/app/app.component.ts
@@ -1,4 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+
+@Component({
+  selector: 'member',
+  template: `
+  <p [style.backgroundColor]="getBackgroundColor()">
+    <ng-content></ng-content>
+  </p>
+  `,
+  styles: ["p{padding: 5px}"]
+})
+export class MemberComponent {
+  selected = false;
+
+  getBackgroundColor() {
+    return this.selected ? "#FFCCCC" : "#CCFFFF";
+  }
+}
+
+@Component({
+  selector: 'crew',
+  template: `
+  <p>
+    <ng-content></ng-content>
+  </p> 
+  `,
+  styles: []
+})
+export class CrewComponent implements AfterContentInit {
+  @ContentChildren(MemberComponent) members: QueryList<MemberComponent>;
+  
+  ngAfterContentInit() {
+    this.members.first.selected = true;
+  }
+}
 
 @Component({
   selector: 'app-root',
--- d141400/src/app/app.module.ts
+++ d141400/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent, CrewComponent, MemberComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    CrewComponent, 
+    MemberComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]

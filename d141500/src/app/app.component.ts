5921f81ee467ca955a7e60ade6def74cece6cf8c
anonymoussc <50c5ac69@opayq.com>
2017-08-14 18:57:35 +0700

    Updated d141500

--- d141500/src/app/app.component.ts
+++ d141500/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, AfterContentChecked } from '@angular/core';
+
+@Component({
+  selector: 'card',
+  template: `
+  <ng-content></ng-content>
+  `,
+  styles:[]
+})
+export class CardComponent {
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent implements AfterContentChecked {
+  card = CARD_ACE_OF_SPADES;
+
+  @ContentChild(CardComponent) contentChild: CardComponent;
+
+  ngAfterContentChecked() {
+    console.log("content inside card has been checked: " + this.card); 
+  }
+  
+  pickCard() {
+    this.card = this.card === CARD_ACE_OF_SPADES ? CARD_TEN_OF_CLUBS : CARD_ACE_OF_SPADES; 
+  }
+} 
+  
+const CARD_ACE_OF_SPADES = 'ace of spades';
+const CARD_TEN_OF_CLUBS = 'ten of clubs';
+

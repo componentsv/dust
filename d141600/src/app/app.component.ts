d2a5cec0688357d3dd7b7e218a3b8c080e74098d
anonymoussc <50c5ac69@opayq.com>
2017-08-14 19:11:42 +0700

    Updated d141600

--- d141600/src/app/app.component.ts
+++ d141600/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('firstInput') firstInput;
+
+  ngAfterViewInit(){
+    // ViewChild variables are available in this method.
+    // Set initial focus.
+    this.firstInput.nativeElement.focus(); 
+  }
 }

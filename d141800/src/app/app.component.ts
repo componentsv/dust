6a7be90759c5cf99a3ea8cc1bbbbd2fbcb7afd4e
anonymoussc <50c5ac69@opayq.com>
2017-08-15 19:26:19 +0700

    Updated d141800

--- d141800/src/app/app.component.ts
+++ d141800/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  interval;
+  count = 0;
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      this.count++;
+    })
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+    delete this.interval;
+  }
 }

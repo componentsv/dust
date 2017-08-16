c4aa0f358a2d0441dbd52d5ac2a7f0bf7cc620e7
anonymoussc <50c5ac69@opayq.com>
2017-08-17 05:03:54 +0700

    Updated d15500

--- d15500/src/app/app.component.ts
+++ d15500/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Card } from './card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{
+    provide: Card,
+    useFactory: () => {
+      const suite: number = Math.floor(Math.random() * 4);
+      const suiteName: string = suite == 0 ? "Clubs" : suite == 1 ? "Diamonds" : suite == 2 ? "Hearts" : "Spades";
+      const rank: number = Math.floor(Math.random() * 15);
+      const rankName: string = rank == 0 ? "Ace" : rank == 1 ? "Joker" : rank == 2 ? "King" : rank == 3 ? "Queen" : (rank - 3).toString();
+      
+      return new Card(suiteName, rankName);
+    }
+  }]
 })
 export class AppComponent {
+  constructor(card: Card) {
+    this.title = card.toString();
+  }
   title = 'app';
 }

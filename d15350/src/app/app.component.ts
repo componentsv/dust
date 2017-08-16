cfcc68724702ddae204c2c9551f43ee3015b0c4b
anonymoussc <50c5ac69@opayq.com>
2017-08-17 02:36:41 +0700

    Updated d15350

--- d15350/src/app/app.component.ts
+++ d15350/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+class Watch {
+  getTime(): string {
+    return new Date() + "";
+  }
+}
+
+class Seiko extends Watch {
+  getTime(): string {
+    return "Seiko Time:" + super.getTime();
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{
+    provide: Watch,
+    useClass: Seiko
+  }]
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private watch: Watch) { }
 }

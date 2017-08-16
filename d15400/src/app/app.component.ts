c7668faa2c828ec5254dd74c931cac5f53d93d05
anonymoussc <50c5ac69@opayq.com>
2017-08-17 04:01:00 +0700

    Updated d15400

--- d15400/src/app/app.component.ts
+++ d15400/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [provideLoggingService()]
 })
 export class AppComponent {
+  constructor(private logging: LoggingService) {
+    logging.log('test log');
+  }
   title = 'app';
+}
+
+export const LOGGING_USE_DATE = false;
+export function provideLoggingService() {
+  return {
+    provide: LoggingService,
+    useFactory: () => new LoggingService(LOGGING_USE_DATE)
+  }
 }

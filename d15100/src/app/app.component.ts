1fcf5546ddb52546bb4a2a46dd450ae9447f6f86
anonymoussc <50c5ac69@opayq.com>
2017-08-16 16:51:19 +0700

    Updated d15100

--- d15100/src/app/app.component.ts
+++ d15100/src/app/app.component.ts
@@ -1,4 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CarService } from './car.service';
+
+@Component({
+  selector: 'car',
+  template: `
+    <h3>
+      {{name}} Is Supercharged: {{supercharged}}
+    </h3>
+  `,
+  styles: [],
+  providers: [CarService]
+  })
+  export class CarComponent implements OnInit {
+    @Input() name;
+
+    supercharged: string = '';
+
+    constructor(private service: CarService) {}
+    
+    ngOnInit(){
+      this.supercharged = this.service.isSuperCharged(this.name);
+    }
+  }
 
 @Component({
   selector: 'app-root',

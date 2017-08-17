6ab9ea04c6577ad266b8f42dfce0534e2e0cb271
anonymoussc <50c5ac69@opayq.com>
2017-08-18 06:11:56 +0700

    Updated d17100

--- d17100/src/app/app.component.ts
+++ d17100/src/app/app.component.ts
@@ -1,4 +1,33 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+
+@Component({
+  selector: 'pepperoni',
+  template: `
+    <h2>Pepperoni</h2>
+    <img src="https://thumb1.shutterstock.com/display_pic_with_logo/55755/161642033/stock-photo-single-slice-of-pepperoni-meat-isolatedon-white-with-path-shot-from-above-161642033.jpg">
+  `,
+  styles: []
+})
+export class PepperoniComponent {
+}
+
+@Component({
+  selector: 'everything',
+  template: `
+    <h2>Everything</h2>
+    Size:{{size}}
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0UXyx2jQrCBBBw2N4ofFVw2oWz7keZjDVUB4UDrASE9JHwQdi">
+  `,
+  styles: []
+})
+export class EverythingComponent {
+  private size: String = '';
+
+  constructor(private route: ActivatedRoute) {
+    route.params.subscribe((params: Object) => this.size = params['size']);
+  }
+}
 
 @Component({
   selector: 'app-root',
--- d17100/src/app/app.module.ts
+++ d17100/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, PepperoniComponent, EverythingComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PepperoniComponent,
+    EverythingComponent
   ],
   imports: [
     BrowserModule,

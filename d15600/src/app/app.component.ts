326f29883584cd718f7c59071dce998754a396c1
anonymoussc <50c5ac69@opayq.com>
2017-08-17 14:57:28 +0700

    Updated d15600

--- d15600/src/app/app.component.ts
+++ d15600/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{
+    provide: 'language',
+    useValue: 'en'
+  }]
 })
 export class AppComponent {
-  title = 'app';
+  title: string = '';
+  
+  constructor(private injector: Injector) {
+    console.log(injector);
+    this.title = 'Language is: ';
+  }
 }

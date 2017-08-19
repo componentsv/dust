0cac62e3a7cf5743d416812d614069f5e1482d0d
anonymoussc <50c5ac69@opayq.com>
2017-08-19 07:26:01 +0700

    Updated d17300

--- d17300/src/app/app.component.ts
+++ d17300/src/app/app.component.ts
@@ -1,4 +1,41 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CustomerService } from './customerService';
+import { Customer } from './customer';
+
+@Component({
+  selector: 'pleaseSelect',
+  template: `
+    <div>
+      <h2>Please make a selection.</h2>
+    </div>
+  `,
+  styles: ['div { background-color: #FFFFFF; padding: 10px; border: 1px solid #000000 }']
+})
+export class PleaseSelectComponent {
+}
+
+@Component({
+  selector: 'detail',
+  template: `
+      <div>
+        <h2>Customer Detail {{id}}</h2> 
+        <p>{{customer.name}}</p>
+        <p>
+          {{customer.city}}, {{customer.state}}
+        </p> 
+        <p>Balance: &#36;{{customer.balance}}</p> 
+      </div>
+    `,
+  styles: ['div { background-color: #FFE4E1 }']
+})
+export class DetailComponent {
+  customer: Customer;
+
+  constructor(private customerService: CustomerService, private route: ActivatedRoute) {
+    route.queryParams.subscribe((queryParams: Object) => this.customer = customerService.getCustomerById(queryParams['id']));
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +43,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private _customerService: CustomerService){
+  }
 }
--- d17300/src/app/app.module.ts
+++ d17300/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'; 
+import { AppComponent, DetailComponent, PleaseSelectComponent } from './app.component'; 
+import { CustomerService } from './customerService';
+import { Customer } from './customer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent,
+    PleaseSelectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

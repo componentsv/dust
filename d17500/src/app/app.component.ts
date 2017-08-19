b4e525e63e7e61da10960c2746e6bc942cd55d07
anonymoussc <50c5ac69@opayq.com>
2017-08-19 19:30:20 +0700

    Updated d17500

--- d17500/src/app/app.component.ts
+++ d17500/src/app/app.component.ts
@@ -1,4 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'non-authenticated-component',
+  template: `
+  <div>
+    <h2>Non-authenticated</h2>
+    <p>This component can be accessed without authentication.</p>
+  </div>
+  `,
+  styles: []
+})
+export class NonAuthenticatedComponent { }
+
+@Component({
+  selector: 'authenticated-component',
+  template: `
+    <div>
+      <h2>Authenticated</h2>
+      <p>This component cannot be accessed without authentication.</p>
+     </div>
+  `,
+  styles: []
+})
+export class AuthenticatedComponent { }
 
 @Component({
   selector: 'app-root',
@@ -6,5 +31,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  loggedIn: boolean = false;
+  
+  @ViewChild('name') name;
+  @ViewChild('password') password;
+  
+  constructor(private _userService: UserService) { }
+  
+  login() {
+    this._userService.authenticate(
+      this.name.nativeElement.value,
+      this.password.nativeElement.value);
+  }
 }
--- d17500/src/app/app.module.ts
+++ d17500/src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, AuthenticatedComponent, NonAuthenticatedComponent } from './app.component'; 
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticatedComponent,
+    NonAuthenticatedComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

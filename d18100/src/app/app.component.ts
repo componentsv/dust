05877bbf634c47225d208078b150823139545b1b
anonymoussc <50c5ac69@opayq.com>
2017-08-20 18:22:14 +0700

    Updated d18100

--- d18100/src/app/app.component.ts
+++ d18100/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Rx from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+
+    const array: Array<string> = ['event1', 'event2'];
+
+    const observable: Rx.Observable<string[]> = Rx.Observable.of(array);
+
+    const subscription: Rx.Subscription = observable.subscribe(
+      // Observer
+      function (x) {
+        console.log('Next: ' + x);
+      },
+      function (err) {
+        console.log('Error: ' + err);
+      },
+      function () {
+        console.log('Completed');
+      });
+  }
 }

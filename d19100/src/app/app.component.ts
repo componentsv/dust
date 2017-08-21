51ef5da0a5fadef563145946e5da53bbe9b62d3f
anonymoussc <50c5ac69@opayq.com>
2017-08-21 19:07:01 +0700

    Updated d19100

--- d19100/src/app/app.component.ts
+++ d19100/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Rx from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  _logs: Array<string> = [];
+
+  constructor() {
+    const observable: Rx.Observable<any> = Rx.Observable.merge(
+      Rx.Observable.fromEvent(document, 'keydown'), 
+      Rx.Observable.fromEvent(document, 'click'),
+      Rx.Observable.fromEvent(document, 'mousemove'), 
+      Rx.Observable.fromEvent(document, 'scroll'),
+      Rx.Observable.fromEvent(document, 'touchstart')
+    );
+
+    const idleEventObservable = observable.bufferTime(5000).filter(function (arr) {
+      return arr.length == 0;
+    })
+      .subscribe(idleEvent => this._logs.push('idle'));
+  }
 }

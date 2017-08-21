fb16e21bd3726eb59af44001bdab6cc08fe586ab
anonymoussc <50c5ac69@opayq.com>
2017-08-22 04:21:38 +0700

    Updated d20200

--- d20200/src/app/app.component.ts
+++ d20200/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers, URLSearchParams, RequestOptions, RequestMethod } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  _search = 'Atlanta';
+  _result = '';
+
+  constructor(private _http: Http) { }
+
+  doSearchConcatenatedUrl() {
+    const mashapeKey = 'OxWYjpdztcmsheZU9AWLNQcE9g9wp1qdRkFjsneaEp2Yf68nYH';
+    const headers: Headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('X-Mashape-Key', mashapeKey);
+
+    const options = new RequestOptions({ headers: headers });
+
+    // concatenated string
+    const concatenatedUrl: string = "https://trailapi-trailapi.p.mashape.com?q[city_cont]=" + encodeURIComponent(this._search);
+
+    return this._http.get(concatenatedUrl, options)
+      .subscribe(res => { this._result = JSON.stringify(res.json()); })
+  }
+
+  doSearchOptionParameters() {
+    const mashapeKey = 'OxWYjpdztcmsheZU9AWLNQcE9g9wp1qdRkFjsneaEp2Yf68nYH';
+    const headers: Headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('X-Mashape-Key', mashapeKey);
+
+    const params: URLSearchParams = new URLSearchParams();
+
+    params.append('q[city_cont]', this._search);
+
+    // params object
+    const options = new RequestOptions({ headers: headers, params: params });
+
+    return this._http.get("https://trailapi-trailapi.p.mashape.com?q[city_cont]=" + encodeURIComponent(this._search), options)
+      .subscribe(res => { this._result = JSON.stringify(res.json()); })
+  }
+
+  doSearchOptionSearch() {
+    const mashapeKey = 'OxWYjpdztcmsheZU9AWLNQcE9g9wp1qdRkFjsneaEp2Yf68nYH';
+    const headers: Headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('X-Mashape-Key', mashapeKey);
+
+    const params: URLSearchParams = new URLSearchParams();
+    params.append('q[city_cont]', this._search);
+
+    // search object
+    const options = new RequestOptions({ headers: headers, search: params });
+
+    return this._http.get("https://trailapi-trailapi.p.mashape.com?q[city_cont]=" + encodeURIComponent(this._search), options)
+      .subscribe(res => { this._result = JSON.stringify(res.json()); })
+  }
 }
--- d20200/src/app/app.module.ts
+++ d20200/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

ff5f33180008fe961b389583caf273f0a285979a
anonymoussc <50c5ac69@opayq.com>
2017-08-21 10:34:06 +0700

    Updated d18300

--- d18300/src/app/app.component.ts
+++ d18300/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Rx from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  _searchText: string;
+  _searchSubject: Rx.Subject<string>;
+  _logs: Array<string> = [];
+  
+  constructor() {
+    // Create new Subject.
+    this._searchSubject = new Rx.Subject<string>();
+    // Set the Subject up to subscribe to events and filter them by // debounce events and ensure they are distinct.
+    this._searchSubject
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .subscribe(
+      // Handle event. Log it.
+      searchText => this._logs.push(searchText));
+  }
+
+  public onChange(searchText: string) {
+    // Emit an event to the Subject.
+    this._searchSubject.next(searchText);
+  }
 }

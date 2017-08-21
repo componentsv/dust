299775de6c80c0eb6dda09872c35566e3a969a75
anonymoussc <50c5ac69@opayq.com>
2017-08-22 04:47:46 +0700

    Updated d20300

--- d20300/src/app/app.component.ts
+++ d20300/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Http, Headers, URLSearchParams, RequestOptions, RequestMethod } from '@angular/http';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  _posts = [];
+  _post = {};
+
+  @ViewChild('modal') _myModal: any;
+  constructor(private _http: Http) {
+  }
+
+  ngOnInit() {
+    return this._http.get("http://jsonplaceholder.typicode.com/posts").subscribe(
+      res => {
+        this._posts = res.json();
+      });
+  }
+
+  showPost(postId: number) {
+    this._http.get(`http://jsonplaceholder.typicode.com/posts/${postId}`).subscribe(
+      res => {
+        this._post = res.json();
+        this._myModal.nativeElement.style.display = 'block';
+      }
+    )
+  }
+
+  closeModal() {
+    this._myModal.nativeElement.style.display = 'none';
+  }
+}
 }

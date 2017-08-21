ce507e434c5489eb9300faf9b9f0abc8a2b51f30
anonymoussc <50c5ac69@opayq.com>
2017-08-22 03:53:06 +0700

    Updated d20100

--- d20100/src/app/app.component.ts
+++ d20100/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwaggerService } from './swagger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SwaggerService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  _languages = new Array<any>();
+
+  constructor(private _swaggerService: SwaggerService) { }
+
+  ngOnInit() {
+    this._swaggerService.getLanguages().subscribe(
+      res => { this._languages = res.json(); },
+      error => { console.log('an error occurred'); }
+    )
+  }
 }

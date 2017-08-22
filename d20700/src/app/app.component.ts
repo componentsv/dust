0fc34cb924f803c36c7d25c6ed48c42be575d550
anonymoussc <50c5ac69@opayq.com>
2017-08-22 23:05:46 +0700

    Updated d20700

--- d20700/src/app/app.component.ts
+++ d20700/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  _result: any;
+
+  constructor(private _http: Http) { }
+  
+  ngOnInit() {
+    this._result = this._http.get("http://jsonplaceholder.typicode.com/posts")
+      .map(response => response.json())
+      .map(response => {
+        let titles = '';
+
+        for (const responseItem of response) {
+          titles += responseItem['title'];
+        }
+
+        return titles;
+      });
+  }
 }

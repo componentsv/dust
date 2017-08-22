fc21d4ee1d2d4f55d79a7304bf2001f0a8886565
anonymoussc <50c5ac69@opayq.com>
2017-08-23 06:19:49 +0700

    Updated d21200

--- d21200/src/app/app.component.ts
+++ d21200/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm, RequiredValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('appointmentForm') _appointmentForm: NgForm; 
+  
+  _name: string = 'mark';
+  _password: string = '';
+  _time: string = '';
+  _ailment: string = '';
+
+  onSubmitForm() {
+    alert("Submitting data:" + JSON.stringify(this._appointmentForm.value));
+  }
 }

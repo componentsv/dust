26dcac15da1be56a4f82da1c05fbb148e2fb41c3
anonymoussc <50c5ac69@opayq.com>
2017-08-22 07:59:46 +0700

    Updated d20400

--- d20400/src/app/app.component.ts
+++ d20400/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Http, Headers, URLSearchParams, RequestOptions, RequestMethod } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  _title: string;
+  _body: string;
+  _added: Array<any> = new Array<any>();
+
+  constructor(private _http: Http) { }
+
+  onAdd() {
+    const requestBody = {
+      title: this._title || '[Unspecified]', body: this._body || '[Unspecified]',
+    };
+    this._http.post("http://jsonplaceholder.typicode.com/posts", requestBody).subscribe(
+      res => {
+        this._added.push(res.json());
+      })
+  }
 }

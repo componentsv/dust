800335cd798575011c2513134118d90649e8cd08
anonymoussc <50c5ac69@opayq.com>
2017-08-22 15:25:49 +0700

    Updated d20500

--- d20500/src/app/app.component.ts
+++ d20500/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Post } from './Post';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  _posts: Array<Post>;
+
+  constructor(private _http: Http) { }
+
+  ngOnInit() {
+    return this._http.get("http://jsonplaceholder.typicode.com/posts").map(response => response.json())
+      .map(
+      response => {
+        const postsArray: Array<Post> = new Array<Post>();
+
+        for (const responseItem of response) {
+          const post = new Post(responseItem['title'], responseItem['body']);
+          postsArray.push(post);
+        }
+
+        return postsArray;
+      })
+      .subscribe(response => { this._posts = response; });
+  }
 }

d9c9116998e5ae9c74e3acd0015c628485800d7c
anonymoussc <50c5ac69@opayq.com>
2017-08-24 04:37:48 +0700

    Add d22200

--- d22200/src/app/app.component.ts
+++ d22200/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReversePipe } from './reverse.pipe';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name: string = 'Orlandeau tgc';
 }

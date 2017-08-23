961cc73fc5216c098407f3cc962461801218653a
anonymoussc <50c5ac69@opayq.com>
2017-08-23 16:58:11 +0700

    Updated d21600

--- d21600/src/app/app.component.ts
+++ d21600/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, FormControl, FormControlName, Validators } from '@angular/forms';
+
+export function validateNotMercedes(control: AbstractControl) {
+  return (control.value.toLowerCase() != 'mercedes') ?
+    null :
+    {
+      validateNotMercedes: {
+        valid: false
+      }
+    }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  formGroup: FormGroup;
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      make: new FormControl('', [Validators.required, validateNotMercedes]), model: new FormControl('', Validators.required)
+    });
+  } 
+  
+  onSubmit(form: FormGroup) {
+    alert('submit');
+  }
 }

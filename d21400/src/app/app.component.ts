3d384e8d9aeeea70e96f7dae0401821d82513636
anonymoussc <50c5ac69@opayq.com>
2017-08-23 14:29:21 +0700

    Updated d21400

--- d21400/src/app/app.component.ts
+++ d21400/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormControlName, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  formGroup: FormGroup;
+  
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      name: new FormControl('', Validators.required), location: new FormControl('', Validators.required)
+    });
+  } 
+  
+  onSubmit(form: FormGroup) {
+    alert('sumit');
+  }
 }

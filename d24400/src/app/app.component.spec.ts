104a6e04246fe0b0bb97a0259d9f6d55021836b2
anonymoussc <50c5ac69@opayq.com>
2017-08-24 17:30:16 +0700

    Updated d24400

--- d24400/src/app/app.component.spec.ts
+++ d24400/src/app/app.component.spec.ts
@@ -1,14 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ReflectiveInjector } from '@angular/core';
+import { TestBed, tick, async } from '@angular/core/testing';
+import { HttpModule, Response, ResponseOptions, ConnectionBackend, BaseRequestOptions, RequestOptions, Http } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { fakeBackendWithDataProvider } from './fakeBackendWithData';
+import { fakeBackendWithNoDataProvider } from './fakeBackendWithNoData';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Observable } from 'rxjs/Rx';
 
-describe('AppComponent', () => {
+describe('AppComponent (data found)', () => {
+
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        HttpModule,
+        FormsModule
+      ],
+      providers: [
+        fakeBackendWithDataProvider,
+        MockBackend,
+        BaseRequestOptions        
+      ]
     }).compileComponents();
+
   }));
 
   it('should create the app', async(() => {
@@ -17,16 +36,58 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+  it('should render the first trail found', async(() => {
 
-  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    compiled.querySelector('button').click();
+    setTimeout(() => {
+      expect(compiled.querySelector('#name').textContent).toContain('Boat Rock');
+      expect(compiled.querySelector('#state').textContent).toContain('Georgia');
+      expect(compiled.querySelector('#directions').textContent).toContain('Interstate 20 and Fulton Industrial Boulevard');
+      expect(compiled.querySelector('#activities').children.length).toBe(1);
+    }, 2000);
   }));
+
+});
+
+describe('AppComponent (no data found)', () => {
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      imports: [
+        HttpModule,
+        FormsModule
+      ],
+      providers: [
+        fakeBackendWithNoDataProvider,
+        MockBackend,
+        BaseRequestOptions        
+      ]
+    }).compileComponents();
+
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+  it('should render an error message', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('button').click();
+    fixture.detectChanges();
+    setTimeout(() => {
+      expect(compiled.querySelector('#notFound').textContent).toContain('We could not find a trail here');
+    }, 2000);
+  }));
+
 });
--- d24400/src/app/app.component.ts
+++ d24400/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Http, Headers, URLSearchParams, RequestOptions, RequestMethod } from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  _search = 'Atlanta';
+  _searched = false;
+  _result = '';
+
+  constructor(private _http: Http) { }
+
+  doSearch() {
+    this.search(this._search).subscribe(
+      res => {
+        const obj = res.json();
+        if ((obj) && (obj.places) && (obj.places.length) && (obj.places.length > 0)) {
+          this._result = obj.places[0];
+        } else {
+          this._result = undefined;
+        }
+      }
+      ,
+      err => {
+        console.log(err);
+      },
+      () => {
+        this._searched = true;
+      });
+  }
+
+  search(search) {
+    const mashapeKey = 'OxWYjpdztcmsheZU9AWLNQcE9g9wp1qdRkFjsneaEp2Yf68nYH';
+    const headers: Headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('X-Mashape-Key', mashapeKey);
+    const options = new RequestOptions({
+      headers: headers
+    });
+
+    // concatenated string
+    const concatenatedUrl: string = "https://trailapi-trailapi.p.mashape.com?q[city_cont]=" + encodeURIComponent(search);
+    return this._http.get(concatenatedUrl, options);
+  }
 }

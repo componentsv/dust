05287a1a5c6c6f34283968551e5aaac462c5c0de
anonymoussc <50c5ac69@opayq.com>
2017-08-24 15:58:57 +0700

    Updated d24200

--- d24200/src/app/app.component.spec.ts
+++ d24200/src/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed, async } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -14,19 +13,36 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title '0'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+
+    expect(app.counter).toEqual(0);
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it(`should render '0' in a h1 tag`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+    expect(compiled.querySelector('h1').textContent).toContain('0');
+  }));
+
+  it('should increment counter ten times', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    for (let i = 0; i < 10; i++) {
+      compiled.querySelector('button').click();
+      fixture.detectChanges();
+      const nbrStr = (i + 1) + '';
+
+      expect(compiled.querySelector('h1').textContent).toContain(nbrStr);
+    }
   }));
 });

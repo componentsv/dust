072cf9b13b2b1b9825c8e54285966b22823af51a
anonymoussc <50c5ac69@opayq.com>
2018-05-30 00:37:26 +0700

    refactor(d13200): Reformat Modules
    Reformat app.component { .css, .html, .specs.ts, .ts}
    
    BREAKING CHANGE: None
    
    Closes None

--- d13200/src/app/app.component.ts
+++ d13200/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector   : 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls  : ['./app.component.css']
 })
 export class AppComponent {
   names = [
-    'Peter Falk', 'Mary-Ann Blige', 'Eminem'
-  ]
+    'Peter Dinklage', 'Mary-Ann J Blige', 'Eminem'
+  ];
 }

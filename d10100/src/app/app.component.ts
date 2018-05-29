5bcb818d48a078d76693fa674c66115e89cb68a1
anonymoussc <50c5ac69@opayq.com>
2018-05-29 21:21:46 +0700

    refactor(d10100): Reformat app.component
    Reformat app.component {.html, .ts}
    
    BREAKING CHANGE: None
    
    Closes None

--- d10100/src/app/app.component.ts
+++ d10100/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector   : 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls  : ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
 
-  getReversed(str:string) {
+  getReversed(str: string) {
     let reversed = '';
-    
-    for (let i=str.length-1; i>=0;i--) {
-      reversed += str.substring(i, i+1);
+
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str.substring(i, i + 1);
     }
 
     return reversed;

c86c8c81fc66ff8beafe8aea59ef8466327584c8
anonymoussc <50c5ac69@opayq.com>
2018-05-30 00:51:25 +0700

    refactor(d13400): Reformat Modules
    Reformat app.component { .css, .html, .specs.ts, .ts}
    
    BREAKING CHANGE: None
    
    Closes None

--- d13400/src/app/app.component.ts
+++ d13400/src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector   : 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls  : ['./app.component.css']
 })
 export class AppComponent {
   selectedAnimal = 'cat';
-  animals = ['cat', 'dog', 'zebra', 'giraffe'];
+  animals        = ['cat', 'dog', 'zebra', 'giraffe'];
 
-  onAnimalClicked(event:Event) {
+  onAnimalClicked(event: Event) {
     const clickedAnimal = event.srcElement.innerHTML.trim();
-    
+
     this.selectedAnimal = clickedAnimal;
   }
 }

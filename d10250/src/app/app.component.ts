7443f37dd33caad3a26647c2dcd68b5608f1119c
anonymoussc <50c5ac69@opayq.com>
2018-05-29 21:45:24 +0700

    refactor(d10250): Reformat app.component
    Reformat app.component {.html, .specs.ts, .ts}
    
    BREAKING CHANGE: None
    
    Closes None

--- d10250/src/app/app.component.ts
+++ d10250/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import {Car} from './car';
+import { Car } from './car';
 
 @Component({
-  selector: 'app-root',
+  selector   : 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls  : ['./app.component.css']
 })
 export class AppComponent {
   _cars = [
-    new Car('car1', 2002, 'bmw', 'm3', 'https://en.wikipedia.org/wiki/BMW_M3'), 
-    new Car('car2', 2017, 'acura', 'nsx', 'https://en.wikipedia.org/wiki/Honda_NSX'), 
+    new Car('car1', 2002, 'bmw', 'm3', 'https://en.wikipedia.org/wiki/BMW_M3'),
+    new Car('car2', 2017, 'acura', 'nsx', 'https://en.wikipedia.org/wiki/Honda_NSX'),
     new Car('car3', 2016, 'chevy', 'camaro', 'https://en.wikipedia.org/wiki/Chevrolet_Camaro')
   ];
 
@@ -17,7 +17,7 @@ export class AppComponent {
     const desc = event.target.parentElement.dataset.desc;
 
     if (window.confirm('if you click "ok" you would be redirected to an article about the' + desc + '. Cancel will load this website')) {
-      window.location.href=event.target.parentElement.dataset.article;
+      window.location.href = event.target.parentElement.dataset.article;
     }
   }
 }

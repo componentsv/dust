363eb8aadfb55321d1cc2e5fbdf21cc47348c724
anonymoussc <50c5ac69@opayq.com>
2018-06-02 12:06:01 +0700

    refactor(d141200): Reformat app.component
    Reformat app.component { .specs.ts, .ts, .html, .css }
    
    BREAKING CHANGE: None
    
    Closes None

--- d141200/src/app/app.component.ts
+++ d141200/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector   : 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls  : ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   logs: Array<string> = [new Date() + ''];
 
   constructor() {
-    for (let i=0; i < 1000; i++) {
+    for (let i = 0; i < 1000; i++) {
       console.log(i);
     }
   }
